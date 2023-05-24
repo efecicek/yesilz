7e08d920e3d9c4800190c8e211d05edc5601f78f
efecicek <123986408+efecicek@users.noreply.github.com>
2023-05-24 21:38:18 +0300

    iletişim bölümü son düzenlemeler ve diğer sayfalarda bazı eklemeler

--- js/script.js
+++ js/script.js
@@ -1,32 +1,76 @@
 window.addEventListener('load',  e => {
-    var adres = document.getElementById("adres")
-    var email1 = document.getElementById("email1")
     var telNumber = document.getElementById("tel")
-
     var interval = setInterval (function(){
-        var telNumber = document.getElementById("tel")
-        telNumber.style.opacity = Number(telNumber.style.opacity) + 0.1
+        telNumber.style.opacity = Number(telNumber.style.opacity) + 0.01
         if(telNumber.style.opacity ==="1"){
             clearInterval(interval)
         }
     
-    },200)
-    
+    },40)
+
+    var telNumber = document.getElementById("tel")
+    var x = -225
+    var interval = setInterval(function() {
+        telNumber.style.left = x + "px"
+        x += 10
+        if (x >= 25) {
+            clearInterval(interval)
+        }
+    }, 50)
+})
+
+//telefon kısmı
+
+const myTimeout1 = setTimeout(wait2seconds1, 2000);
+
+function wait2seconds1() {
+    var adres = document.getElementById("adres")
     var interval = setInterval (function(){
-        var adres = document.getElementById("adres")
-        adres.style.opacity = Number(adres.style.opacity) + 0.1
+        adres.style.opacity = Number(adres.style.opacity) + 0.01
         if(adres.style.opacity ==="1"){
             clearInterval(interval)
         }
-    
-    },200)
+        
+    },40)
 
+    var adres = document.getElementById("adres")
+    var y = -225
+    var interval = setInterval(function() {
+        adres.style.left = y + "px"
+        y += 10
+        if (y >= 25) {
+            clearInterval(interval)
+        }
+    }, 50)
+}
+
+
+//adres kısmı
+
+const myTimeout2 = setTimeout(wait2seconds2, 4000);
+
+function wait2seconds2() {
+    var email1 = document.getElementById("email1")
     var interval = setInterval (function(){
-        var email1 = document.getElementById("email1")
-        email1.style.opacity = Number(email1.style.opacity) + 0.1
+        email1.style.opacity = Number(email1.style.opacity) + 0.01
         if(email1.style.opacity ==="1"){
             clearInterval(interval)
         }
-    
-    },200)
+        
+    },40)
+
+    var email1 = document.getElementById("email1")
+    var z = -225
+    var interval = setInterval(function() {
+        email1.style.left = z + "px"
+        z += 10
+        if (z >= 25) {
+            clearInterval(interval)
+        }
+    }, 50)
+}
+//email kısmı
+
+document.getElementById("button").addEventListener("click", function() {
+    alert("Mesaj gönderme sistemimiz yapım aşamasındadır. Anlayışınız için teşekkürler.")
 })
