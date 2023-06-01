fc2c329e3f5aa1c8bd4457b8bb0fc925762b3c14
efecicek <123986408+efecicek@users.noreply.github.com>
2023-06-01 17:20:36 +0300

    giriş yap ekranı update

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,30 +1,31 @@
 var Signİnİsim = document.getElementById("Signİnİsim")
-var Signİnİsimdiv = document.getElementById("Signİnİsim-div")
 var SignİnEmail = document.getElementById("SignİnEmail")
 var SignİnPassword = document.getElementById("SignİnPassword")
 var SignİnButton = document.getElementById("SignİnButton")
 var SignUpButton = document.getElementById("SignUpButton")
-var SignIn = document.getElementById("Sign-In")
-var SignUp = document.getElementById("Sign-Up")
-
-
-SignIn.addEventListener('click', e => {
-    if(Signİnİsimdiv.style.display === "block"){
-        Signİnİsimdiv.style.display = "none"
-    }
-})
-
-SignUp.addEventListener('click', e => {
-    if(Signİnİsimdiv.style.display === "none"){
-        Signİnİsimdiv.style.display = "block"
-    }
-})
+var SignUpButton2 = document.getElementById("SignUpButton2")
+var LoginButton = document.getElementById("LoginButton")
 
 var isim = Signİnİsim.value
 var email = SignİnEmail.value
 var password = SignİnPassword.value
 
 SignUpButton.addEventListener('click',  e => {
+    LoginButton.style.display = "none"
+    SignUpButton2.style.display = "inline-block"
+    Signİnİsim.style.display = "block"
+})
+
+SignİnButton.addEventListener('click',  e => {
+    Signİnİsim.value = ""
+    SignİnEmail.value = ""
+    SignİnPassword.value = ""
+    SignUpButton2.style.display = "none"
+    LoginButton.style.display = "inline-block"
+    Signİnİsim.style.display = "none"
+})
+
+SignUpButton2.addEventListener('click',  e => {
     if(Signİnİsim.value !== "" && SignİnEmail.value !== "" && SignİnPassword.value !== ""){
         isim = Signİnİsim.value
         email = SignİnEmail.value
@@ -32,8 +33,8 @@ SignUpButton.addEventListener('click',  e => {
     }
 })
 
-SignİnButton.addEventListener('click',  e => {
-    if(Signİnİsim.value !== "" && SignİnEmail.value !== "" && SignİnPassword.value !== "" && email === SignİnEmail.value && password === SignİnPassword.value){
+LoginButton.addEventListener('click',  e => {
+    if(email === SignİnEmail.value && password === SignİnPassword.value){
         alert("Hoşgeldiniz")
     }   else if(SignİnEmail.value !== "" && SignİnPassword.value !== ""){
         alert("gmail veya şifre hatalı")
